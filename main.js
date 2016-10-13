9ea915a5b481ff15af06d2e42dc2de4491248fb8
itivae <Noah@theredkeep.com>
2016-10-13 15:59:42 -0400

    Sass compiles. Simplify Nav. Rename stylesheet.

--- main.js
+++ main.js
@@ -1,47 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactBootstrap from 'react-bootstrap';
-import {Router, Route, Link, browserHistory, IndexRoute} from 'react-router';
+import {Router, Route, RouteHandler, Link, browserHistory, IndexRoute} from 'react-router';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import {Navbar,Nav, NavItem, DropdownButton, MenuItem} from 'react-bootstrap';
+import {LinkContainer,IndexLinkContainer} from 'react-router-bootstrap';
 
 //import App from './App.jsx';
 class App extends React.Component {
 	render() {
 		return (
 			<div>
-				{this.props.children}
+			  <Navbar inverse>
+			    <Navbar.Header>
+			      <Navbar.Brand>
+			        <a href="#">React-Bootstrap</a>
+			      </Navbar.Brand>
+			      <Navbar.Toggle />
+			    </Navbar.Header>
+			    <Navbar.Collapse>
+			      <Nav>
+			      	<IndexLinkContainer to="/">
+			        	<NavItem eventKey={1}>Home</NavItem>
+			        </IndexLinkContainer>
+			        <LinkContainer to="about">
+			        	<NavItem eventKey={2}>About</NavItem>
+			        </LinkContainer>
+			        <LinkContainer to="contact">
+			        	<NavItem eventKey={3}>Contact</NavItem>
+			        </LinkContainer>
+			      </Nav>
+			    </Navbar.Collapse>
+			  </Navbar>
+			  	<div>
+					{this.props.children}
+			    </div>
 			</div>
-		)
+		);
 	}
 }
 
 export default App;
 
-const navbarInstance = (
-  <Navbar inverse>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#">React-Bootstrap</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <NavItem eventKey={1} to="home">Home</NavItem>
-        <NavItem eventKey={2} to="about">About</NavItem>
-        <NavItem eventKey={3} to="contact">Contact</NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
-ReactDOM.render(navbarInstance, document.getElementById('theNav'));
 
 class Home extends React.Component {
 	render() {
 		return(
 			<div>
-				<Banner/>
+				<Banner name="Home" />
 			</div>
 		);
 	}
@@ -53,7 +59,7 @@ class About extends React.Component {
 	render() {
 		return(
 			<div>
-				<h1>About</h1>
+				<Banner name="About" />
 			</div>
 		);
 	}
@@ -64,9 +70,9 @@ class Contact extends React.Component {
 	render() {
 		return(
 			<div>
-				<h1>Contact</h1>
+				<Banner name="Contact" />
 			</div>
-		)
+		);
 	}
 }
 
@@ -74,9 +80,9 @@ class Banner extends React.Component {
 	render() {
 		return(
 			<div className="col-md-12 col-sm-12 banner">
-				<h1>Home</h1>
+				<h1>{this.props.name}</h1>
 			</div>
-		)
+		);
 	}
 }
 
@@ -84,9 +90,9 @@ ReactDOM.render((
 	<Router history = {browserHistory}>
 		<Route path = '/' component = {App}>
 			<IndexRoute component = {Home} />
-			<Route path = "home" component = {Home} />
-			<Route path = "about" component = {About} />
-			<Route path = "contact" component = {Contact} />
+			<Route name="home" path = "home" component = {Home} />
+			<Route name="about" path = "about" component = {About} />
+			<Route name="contact" path = "contact" component = {Contact} />
 		</Route>
 	</Router>
 ), document.getElementById('app'));
